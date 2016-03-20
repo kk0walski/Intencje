adafe41130c5c7c2eee5cbc9e8e853dc070050e1
rolnik25 <karol.kowalski95@gmail.com>
2016-03-20 17:49:46 +0100

    Add countScores

---
+++ app/src/main/java/com/example/android/intencje/CountScore.java
@@ -0,0 +1,13 @@
+package com.example.android.intencje;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CountScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count_score);
+    }
+}

fead6eff5b2bd6a3c5000693c3dd0abd56c742a5
rolnik25 <karol.kowalski95@gmail.com>
2016-03-20 18:50:10 +0100

    add list

--- app/src/main/java/com/example/android/intencje/CountScore.java
+++ app/src/main/java/com/example/android/intencje/CountScore.java
@@ -2,13 +2,16 @@ package com.example.android.intencje;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CountScore extends AppCompatActivity {
 
-    private ArrayList<Score> listItems=new ArrayList<Score>();
+    private ArrayList<Score> listItems = new ArrayList<Score>();
+    private ArrayAdapter<Score> adapter;
     private Bundle score;
 
     @Override
@@ -16,11 +19,16 @@ public class CountScore extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_count_score);
         score = this.getIntent().getExtras();
-
+        add(score);
     }
 
     private void add(Bundle result){
         Score element = new Score(score.getString("nameA"), score.getInt("scoreA"), score.getString("nameB"), score.getInt("scoreB"));
         listItems.add(element);
+    }
+    private void show(){
+        ListView list = (ListView)findViewById(R.id.list);
+        adapter = new ArrayAdapter<Score>(this, R.layout.row,listItems);
+        list.setAdapter(adapter);
     }
 }

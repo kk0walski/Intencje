a60a4921b2482fd52fb873d0ec5becb0895ba8cb
rolnik25 <karol.kowalski95@gmail.com>
2016-03-20 18:38:26 +0100

    try ty get Bundle

--- app/src/main/java/com/example/android/intencje/CountScore.java
+++ app/src/main/java/com/example/android/intencje/CountScore.java
@@ -2,12 +2,25 @@ package com.example.android.intencje;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class CountScore extends AppCompatActivity {
+
+    private ArrayList<Score> listItems=new ArrayList<Score>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_count_score);
+
+        Bundle score = this.getIntent().getExtras();
+
+    }
+
+    private void add(Bundle result){
+        ListView list = (ListView)findViewById(R.id.list);
+        list.
     }
 }
---
+++ app/src/main/java/com/example/android/intencje/Score.java
@@ -0,0 +1,20 @@
+package com.example.android.intencje;
+
+/**
+ * Created by admin on 20.03.2016.
+ */
+public class Score {
+
+    String nameA;
+    int scoreA;
+    String nameB;
+    int scoreB;
+
+    public Score(String aName, int aScore, String bName, int bScore){
+        nameA = aName;
+        scoreA = aScore;
+        nameA = bName;
+        scoreB = bScore;
+    }
+}
+
